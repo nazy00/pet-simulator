26d05187dde3fe2db454a49208cf62ec262cd06a
nazy00 <81646930+nazy00@users.noreply.github.com>
2026-05-14 16:22:50 -0300

    set heart limits

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const heart = document.getElementById("heart");
 
-setInterval(heartDown(), 30000);
+setInterval(heartDown, 3000);
 
 function heartUp() {
     let number = heart.innerHTML;
-    number++;
-    heart.innerHTML = number;
+    if (number < 10) {
+        number++;
+        heart.innerHTML = number;
+    }
 }
 function heartDown() {
     let number = heart.innerHTML;
-    number--;
-    heart.innerHTML = number;
+    if (number > 0) {
+        number--;
+        heart.innerHTML = number;
+    }
 }
